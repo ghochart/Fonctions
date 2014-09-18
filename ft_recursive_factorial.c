06dabde2721719fd79b5a8080e857f6c61a92d6a
ghochart <gabriel.hochart@laposte.net>
2014-09-18 11:02:18 +0200

    Update ft_recursive_factorial.c

--- ft_recursive_factorial.c
+++ ft_recursive_factorial.c
@@ -14,7 +14,7 @@ int ft_recursive_factorial(int nb)
 {
     if (nb == 0)
         return (1);
-    else if (nb < 0 || nb > 13)
+    else if (nb < 0 || nb > 12)
         return (0);
     else
         return (nb * ft_recursive_factorial(nb - 1));
